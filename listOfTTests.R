ec33fc1cd890e0d4420f653c4f136a172efb7518
Leonard Greski <len@ageretechnology.com>
2018-11-23 22:02:56 -0800

    add print section

--- listOfTTests.R
+++ listOfTTests.R
@@ -1,10 +1,16 @@
 #
 # https://stackoverflow.com/questions/53455436/r-error-t-test-for-loop-command-between-variables
 #
-
+data(mtcars)
 varList <- c("wt","disp","mpg")
 results <- lapply(varList,function(x){
   t.test(mtcars[[x]] ~ mtcars$am)
 })
 names(results) <- varList
-results
+
+# print results
+
+for(i in 1:length(results)){
+  message(paste("for variable:",names(results[i]),"difference between manual and automatic transmissions is:"))
+  print(results[[i]])
+}
